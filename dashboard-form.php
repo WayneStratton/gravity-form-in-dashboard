016ad3ee95f12fb0b8ebde7ce7d2fc8c2b655c8e
Wayne <waynestrattonjr@gmail.com>
2017-09-06 23:38:07 -0400

    Update and rename stratton.php to dashboard-form.php

--- dashboard-form.php
+++ dashboard-form.php
@@ -9,20 +9,20 @@
  
 add_action( 'admin_menu', 'get_support_admin_menu' );
  
-//Create an admin menu which will also create a blank admin page
+// Create an admin menu which will also create a blank admin page
 function get_support_admin_menu() {
 	add_menu_page( 'Get Support', 'Get Support', 'manage_options', 'get-support.php', 'gravityforms_admin_page', 'dashicons-thumbs-up', 6  );
 }
  
-//The main function which controls the output on the admin page
+// The main function which controls the output on the admin page
 function gravityforms_admin_page() {
  
 	echo "<div style='width: 60%'>";
 	echo "<h2><span style='font-size: 20px;' class='dashicons dashicons-info'></span> Need help? Use the handy form below and let me know how I can assist you!</h2>";
  
-	gravity_form_enqueue_scripts( 2, true ); //Enqueue form scripts (only for form with ID of 2)
+	gravity_form_enqueue_scripts( 2, true ); // Enqueue form scripts (only for form with ID of 2)
  
-	gravity_form( 2, false, true, false, null, false ); //Output a form with an ID of 2
+	gravity_form( 2, false, true, false, null, false ); // Output a form with an ID of 2
  
 	echo "</div>";
 }
